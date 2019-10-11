6816eb3f6cd753027b57b624ccc227b53b8750f7
Gvozden Kuzman <gvku3008@enjoying.rs>
2019-10-11 08:20:11 +0200

    Add project files.

---
+++ BattleSimulator/Controllers/BattleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BattleSimulator.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BattleController : ControllerBase
+    {
+        [HttpPost]
+        // TODO: strategy to enum
+        public async Task<ActionResult> AddArmy(string name, int units, string strategy)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        // Seems that PUT is the most appropriate one, debatable with POST. Definitely not GET.
+        [HttpPut]
+        public async Task<ActionResult> Start()
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpGet("{battleId}")]
+        public async Task<ActionResult> GetLog(int battleId)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Reset()
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
